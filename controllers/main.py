2bec6da5d4d5310d27ef4a6868dc2e00782a6783
adam <adam@magestore.com>
2017-04-25 11:18:31 +0700

    // Adam: push by Adam

---
+++ controllers/main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import babel.dates
+import re
+import werkzeug
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, http, _
+from odoo.addons.website.models.website import slug
+from odoo.http import request
+
+class LatestBlogPost(http.Controller):
+    @http.route('/snippet/get_latest_blog_post')
+    def get_latest_blog_post(self):
+        BlogPost = self.env['blog.post']
+        result = {"blog_posts":[]}
+        blog_posts = BlogPost.search([])
+
+        for blog_post in blog_posts:
+            create_date_arr = blog_post.create_date.split(" ")[0].split("-")
+            create_date = create_date_arr[2] + "/" + create_date_arr[1] + "/" + create_date_arr[0]
+            result['blog_posts'].append({
+                "blog_post": blog_post,
+                "create_date": create_date
+            })
+        return request.render("latest_blog_post.latest_blog_post_list", result)
---
+++ static/src/js/snippet_small.js
@@ -0,0 +1,12 @@
+odoo.define("latest_blog_post.snippet_small", function(require){
+"use strict";
+var animation = require("web_editor.snippets.animation");
+
+latest_blog_post.registry.animation = animation.Class.extend({
+    $.get('snippet/get_latest_blog_post').then(function(data){
+        if(data){
+            $(".latest_blog_post_list").replaceWith(data)
+        }
+    })
+})
+});
